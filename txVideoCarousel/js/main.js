4186cc4b4ef320039d08f94100775f865e5c1417
南秋SouthAki <xieleihan@gmail.com>
2024-07-27 09:23:36 +0800

    fix:修复一些问题

--- txVideoCarousel/js/main.js
+++ txVideoCarousel/js/main.js
@@ -44,162 +44,91 @@ for (var i = 0; i < li.length; i++){
 
 
 // // 全部定时器
-// var imgBox = document.querySelectorAll('.imgBox');
-
-// var intervalID1 = null;
-// var smallSpan = document.querySelectorAll('.smallSpan');
-// var bigSpan = document.querySelectorAll('.bigSpan');
-// box.style.backgroundColor = data[index].bg;
-// li[0].className = "highlight";
-// // smallSpan[index].style.display = "block";
-// smallSpan[0].style.opacity = "1";
-// bigSpan[0].style.fontWeight = "bold";
-// intervalID1 = setInterval(function () {
-//     index++;
-//     if (index >= imgBox.length) {
-//         index = 0; // 重置索引以循环播放
-//     }
-
-//     for (var j = 0; j < li.length; j++) {
-//         li[j].className = "";
-//         // smallSpan[j].style.display = "none";
-//         smallSpan[j].style.opacity = "0";
-//         bigSpan[j].style.fontWeight = "normal";
-//     }
-//     li[index].className = "highlight";
-//     // smallSpan[index].style.display = "block";
-//     smallSpan[index].style.opacity = "1";
-//     bigSpan[index].style.fontWeight = "bold";
-
-//     // 使用负的百分比值来移动box
-//     box.style.left = -index * 100 + "%";
-//     box.style.backgroundColor = data[index].bg;
-
-// }, 3000);
-// var title = document.querySelector('.title');
-// box.onmouseout = function () {
-//     if (intervalID1 === null) {
-//         intervalID1 = setInterval(function () {
-//             index++;
-//             if (index >= imgBox.length) {
-//                 index = 0; // 重置索引以循环播放
-//             }
-
-//             for (var j = 0; j < li.length; j++) {
-//                 li[j].className = "";
-//                 smallSpan[j].style.opacity = "0";
-//                 bigSpan[j].style.fontWeight = "normal";
-//             }
-//             li[index].className = "highlight";
-//             smallSpan[index].style.opacity = "1";
-//             bigSpan[index].style.fontWeight = "bold";
-
-//             // 使用负的百分比值来移动box
-//             box.style.left = -index * 100 + "%";
-//             box.style.backgroundColor = data[index].bg;
-
-//         }, 3000);
-//     }
-// }
-// box.onmouseover = function () {
-//     clearInterval(intervalID1);
-//     intervalID1 = null;
-// }
-// title.onmouseout = function () {
-//     if (intervalID1 === null) {
-//         intervalID1 = setInterval(function () {
-//             index++;
-//             if (index >= imgBox.length) {
-//                 index = 0; // 重置索引以循环播放
-//             }
-//             for (var j = 0; j < li.length; j++) {
-//                 li[j].className = "";
-//                 smallSpan[j].style.opacity = "0";
-//                 bigSpan[j].style.fontWeight = "normal";
-//             }
-//             li[index].className = "highlight";
-//             smallSpan[index].style.opacity = "1";
-//             bigSpan[index].style.fontWeight = "bold";
-
-//             // 使用负的百分比值来移动box
-//             box.style.left = -index * 100 + "%";
-//             box.style.backgroundColor = data[index].bg;
-
-//         }, 3000);
-//     }
-// }
-// title.onmouseover = function () {
-//     clearInterval(intervalID1);
-//     intervalID1 = null;
-// }
-
-
 var imgBox = document.querySelectorAll('.imgBox');
-var smallSpan = document.querySelectorAll('.smallSpan');
-var bigSpan = document.querySelectorAll('.bigSpan');
-var box = document.querySelector('.box');
-var ul = document.querySelector('ul');
 
 var intervalID1 = null;
-var index = 0;
+var smallSpan = document.querySelectorAll('.smallSpan');
+var bigSpan = document.querySelectorAll('.bigSpan');
+box.style.backgroundColor = data[index].bg;
+li[0].className = "highlight";
+// smallSpan[index].style.display = "block";
+smallSpan[0].style.opacity = "1";
+bigSpan[0].style.fontWeight = "bold";
+intervalID1 = setInterval(function () {
+    index++;
+    if (index >= imgBox.length) {
+        index = 0; // 重置索引以循环播放
+    }
 
-// 复制第一张图片和最后一张图片并添加到末尾和开头
-var firstImgClone = imgBox[0].cloneNode(true);
-var lastImgClone = imgBox[imgBox.length - 1].cloneNode(true);
-ul.appendChild(firstImgClone);
-ul.insertBefore(lastImgClone, ul.firstChild);
-
-function moveToIndex(idx) {
-    box.style.transition = 'left 0.5s';
-    box.style.left = -idx * 100 + '%';
-    box.style.backgroundColor = data[idx % data.length].bg;
-
-    for (var j = 0; j < imgBox.length; j++) {
+    for (var j = 0; j < li.length; j++) {
         li[j].className = "";
+        // smallSpan[j].style.display = "none";
         smallSpan[j].style.opacity = "0";
         bigSpan[j].style.fontWeight = "normal";
     }
-    li[idx % imgBox.length].className = "highlight";
-    smallSpan[idx % imgBox.length].style.opacity = "1";
-    bigSpan[idx % imgBox.length].style.fontWeight = "bold";
-}
+    li[index].className = "highlight";
+    // smallSpan[index].style.display = "block";
+    smallSpan[index].style.opacity = "1";
+    bigSpan[index].style.fontWeight = "bold";
 
-function resetPosition() {
-    if (index === imgBox.length) {
-        box.style.transition = 'none';
-        box.style.left = '0%';
-        index = 0;
+    // 使用负的百分比值来移动box
+    box.style.left = -index * 100 + "%";
+    box.style.backgroundColor = data[index].bg;
+
+}, 3000);
+var title = document.querySelector('.title');
+box.onmouseout = function () {
+    if (intervalID1 === null) {
+        intervalID1 = setInterval(function () {
+            index++;
+            if (index >= imgBox.length) {
+                index = 0; // 重置索引以循环播放
+            }
+
+            for (var j = 0; j < li.length; j++) {
+                li[j].className = "";
+                smallSpan[j].style.opacity = "0";
+                bigSpan[j].style.fontWeight = "normal";
+            }
+            li[index].className = "highlight";
+            smallSpan[index].style.opacity = "1";
+            bigSpan[index].style.fontWeight = "bold";
+
+            // 使用负的百分比值来移动box
+            box.style.left = -index * 100 + "%";
+            box.style.backgroundColor = data[index].bg;
+
+        }, 3000);
     }
-    if (index === -1) {
-        box.style.transition = 'none';
-        box.style.left = -imgBox.length * 100 + '%';
-        index = imgBox.length - 1;
-    }
 }
-
-function startCarousel() {
-    intervalID1 = setInterval(function () {
-        index++;
-        moveToIndex(index);
-    }, 3000);
-}
-
-function stopCarousel() {
+box.onmouseover = function () {
     clearInterval(intervalID1);
     intervalID1 = null;
 }
+title.onmouseout = function () {
+    if (intervalID1 === null) {
+        intervalID1 = setInterval(function () {
+            index++;
+            if (index >= imgBox.length) {
+                index = 0; // 重置索引以循环播放
+            }
+            for (var j = 0; j < li.length; j++) {
+                li[j].className = "";
+                smallSpan[j].style.opacity = "0";
+                bigSpan[j].style.fontWeight = "normal";
+            }
+            li[index].className = "highlight";
+            smallSpan[index].style.opacity = "1";
+            bigSpan[index].style.fontWeight = "bold";
 
-box.style.backgroundColor = data[0].bg;
-li[0].className = "highlight";
-smallSpan[0].style.opacity = "1";
-bigSpan[0].style.fontWeight = "bold";
+            // 使用负的百分比值来移动box
+            box.style.left = -index * 100 + "%";
+            box.style.backgroundColor = data[index].bg;
 
-startCarousel();
-
-box.addEventListener('transitionend', resetPosition);
-
-box.onmouseover = stopCarousel;
-box.onmouseout = startCarousel;
-
-title.onmouseover = stopCarousel;
-title.onmouseout = startCarousel;
+        }, 3000);
+    }
+}
+title.onmouseover = function () {
+    clearInterval(intervalID1);
+    intervalID1 = null;
+}
