da1823a623669f74f40208edaf1e7252a8179cb4
南秋SouthAki <xieleihan@gmail.com>
2024-08-15 16:55:13 +0800

    feat:下午持续更新中,还没喝水

---
+++ 高仿TearsofThemis/js/main.js
@@ -0,0 +1,28 @@
+// Copyright © 2024 SouthAki, All Rights Reserved
+// console.log("该页面仅授权以学习为目的，不得用于商业用途。作者也以学习为目的高仿,并未对原官网公司造成任何损失,本项目开源,因该项目造成的影响由使用者自负。");
+
+// 1.新闻处的标题切换的jQuery部分
+// start
+function getContentNewLi(str) {
+    $.get("../json/content-news.json", function (item) {
+        // console.log(item);
+        // console.log(item[str]);
+        $(".news_list").empty();
+        for (var i = 0; i < item[str].length; i++) {
+            // console.log(item[str][i].info);
+            // console.log(item[str][i].title);
+            // console.log(item[str][i].data);
+            var html = '<li class="news_item"><p class="news_type">' + item[str][i].info + '</p><a href="#" class=".news_content"><span class="news_title">' + item[str][i].title + '</span></a><span class="news_date">' + item[str][i].data + '</span></li > ';
+            $(".news_list").append(html);
+        }
+    });
+}
+getContentNewLi("all");
+$(".news_menu").on("click", "li", function () {
+    $(".news_menu li").removeClass("news_tab-active");
+    $(this).addClass("news_tab-active");
+    // console.log($(this).attr("data-name"));
+    var str = $(this).attr("data-name");
+    getContentNewLi(str);
+});
+// end
