279edf4975803e96e5be2f9e1bd8d5699185490d
南秋SouthAki <xieleihan@gmail.com>
2024-09-11 19:32:26 +0800

    feat:新增内容

---
+++ test.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("hello,world!");
+
+	var a int = 10;
+	var b string = "hello";
+	var c float32 = 3.14;
+	var d bool = true;
+
+	fmt.Println(a,b,c,d);
+}
