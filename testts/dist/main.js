e8a9672a20b306d5b7f4def74492428c8a9775cd
南秋SouthAki <xieleihan@gmail.com>
2024-10-11 15:07:31 +0800

    feat:新增文件

--- testts/dist/main.js
+++ testts/dist/main.js
@@ -1,40 +1,38 @@
-var src = 'src';
-var dest = 'dest';
-console.log(src, dest);
-var num;
-num = 1;
-num = '1';
-console.log(num);
-var anyType;
-anyType = 1;
-console.log(anyType);
-anyType = '这是一个字符串';
-console.log(anyType);
-anyType = true;
-console.log(anyType);
-var div = document.querySelector('body');
-console.log(div);
-var func = function (username) {
-    console.log("你好", username);
-};
-func("GitHub");
-var getRandomNumber = function (min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-};
-console.log(getRandomNumber(1, 100));
-var szy = {
-    username: 'szy',
-    password: '123456',
-};
-var szyinfo = function (user) {
-    console.log(user, user.username, user.password);
-};
-console.log(szy);
-szyinfo({
-    username: 'szy111',
-    password: '123456'
-});
-var str = '1';
-var str2 = str;
-console.log(str2);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var sex = true || false;
+var arr = [{ x: 50, y: 50, color: 'red' }];
+var canvas = document.querySelector('canvas');
+var Car = (function () {
+    function Car(brand, price) {
+        this.brand = brand;
+        this.price = price;
+    }
+    Car.prototype.show = function () {
+        console.log(this.brand, this.price);
+    };
+    return Car;
+}());
+var aodi = (function (_super) {
+    __extends(aodi, _super);
+    function aodi(brand, price) {
+        return _super.call(this, brand, price) || this;
+    }
+    return aodi;
+}(Car));
+var aodi1 = new aodi('奥迪', 1000000);
+aodi1.show();
 //# sourceMappingURL=main.js.map
